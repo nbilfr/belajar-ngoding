ca12eab932ebe60fba34f06d4e1296baea40e628
Nabil Faize R <nabilfaizerabbani@gmail.com>
2023-07-21 13:06:33 +0700

    Create substringwithjumkata.js

---
+++ substringwithjumkata.js
@@ -0,0 +1,19 @@
+let word4 = 'wow JavaScript is so cool';
+
+let exampleFirstWord4 = word4.substring(0, 3);
+let exampleKeduaWord4 = word4.substring(4, 14);
+let exampleKetigaWord4 = word4.substring(15, 17);
+let exampleKeempatWord4 = word4.substring(18, 20);
+let exampleKelimaWord4 = word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+let jumlahkata2 = exampleKeduaWord4.length;
+let jumlahkata3 = exampleKetigaWord4.length;
+let jumlahkata4 = exampleKeempatWord4.length;
+let jumlahkata5 = exampleKelimaWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length : ' + firstWordLength);
+console.log('Kata kedua: ' + exampleKeduaWord4 + ', with length : ' + jumlahkata2);
+console.log('kata ketiga: ' + exampleKetigaWord4 + ', with length : ' + jumlahkata3);
+console.log('kata keempat: ' + exampleKeempatWord4 + ', with length : ' + jumlahkata4);
+console.log('kata kelima: ' + exampleKelimaWord4 + ', with length : ' + jumlahkata5);
